d4f4493631877b233d7dcb66b06d2f230e7e782f
connorsanders <jecsand@pm.me>
2024-03-17 01:30:08 -0500

    Fixed standalone-compose.yml typo

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ docker compose -f docker/standalone-compose.yml up -d
 docker exec -it pulsar /init-pulsar-connectors.sh
 docker exec -it pulsar /init-pulsar-functions.sh
 
-docker exec -it pulsar ./bin/pulsar-client consume -s test-sub -n 0 'persistent://public/default/mongo-topic'
-docker exec -it pulsar ./bin/pulsar-client consume -s test-sub -n 0 'persistent://public/default/ExampleFunction_logs'
+docker exec -it pulsar ./bin/pulsar-client consume -s test-sub3 -n 0 'persistent://public/default/mongo-topic-out'
+docker exec -it pulsar ./bin/pulsar-client consume -s test-sub2 -n 0 'persistent://public/default/ExampleFunction_logs'
 
 ```
 
--- loadTest.py
+++ loadTest.py
@@ -1,5 +1,8 @@
-from pulsar import Function
 import json
+
+data = """{"key":[{"_id": {"$oid": "65f62d575d19b6888c7a48ad"}}], "properties":[], "content":{"fullDocument":{"_id":{"timestamp":1710632279,"date":1710632279000},"breadth":0,"country_name":"Panama","country_code":"PA","call_sign":"3FEI5","cargo":"Cargo","draught":7,"imo":9660607,"length":111,"name":"MEDI MANILA","mmsi":353664000},"ns":{"databaseName":"geoglify","collectionName":"ships","fullName":"geoglify.ships"},"operation":"INSERT"}}"""
+
+print(data)
 
 
 def format_mongo(func):
@@ -7,7 +10,6 @@ def format_mongo(func):
         # Modify the input parameter
         modified_args = list(args)
         if len(modified_args) > 0:
-
             modified_args[0] = f"Modified: {modified_args[0]}"
         # Call the original function with the modified input
         result = func(*modified_args, **kwargs)
@@ -15,13 +17,14 @@ def format_mongo(func):
 
     return wrapper
 
-class ExampleFunction(Function):
+class ExampleFunction():
     def __init__(self):
         pass
 
     def process(self, input, context):
         logger = context.get_logger()
-
         logger.info("Message: {0}".format(input))
         logger.info("Content: {0}".format(input.content))
         return input
+
+print(json.loads(data))
