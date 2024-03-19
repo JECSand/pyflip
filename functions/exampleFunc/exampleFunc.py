a947923ca9ed46d6e79dcf0d11b5b676d51e0649
connorsanders <jecsand@pm.me>
2024-03-18 23:13:22 -0500

    Fixed standalone-compose.yml typo

--- functions/exampleFunc/exampleFunc.py
+++ functions/exampleFunc/exampleFunc.py
@@ -2,26 +2,33 @@ from pulsar import Function
 import json
 
 
-def format_mongo(func):
+def mongo_source(func):
     def wrapper(*args, **kwargs):
-        # Modify the input parameter
         modified_args = list(args)
-        if len(modified_args) > 0:
-
-            modified_args[0] = f"Modified: {modified_args[0]}"
-        # Call the original function with the modified input
+        if len(modified_args) > 1:
+            replaces = [
+                ['key:[', "\"key\":["],
+                [' properties:[', " \"properties\":["],
+                ['content:{"', "\"content\":{\""]
+            ]
+            modified_args[1] = (modified_args[1]
+                                .replace(replaces[0][0], replaces[0][1])
+                                .replace(replaces[1][0], replaces[1][1])
+                                .replace(replaces[2][0], replaces[2][1]))
         result = func(*modified_args, **kwargs)
         return result
 
     return wrapper
 
+
 class ExampleFunction(Function):
     def __init__(self):
         pass
 
+    @mongo_source
     def process(self, input, context):
         logger = context.get_logger()
-
-        logger.info("Message: {0}".format(input))
-        logger.info("Content: {0}".format(input.content))
-        return input
+        data = json.loads(input)
+        logger.info("\nMessage: {0}".format(data))
+        # logger.info("Content: {0}".format(data.get("content")))
+        return json.dumps(data)
